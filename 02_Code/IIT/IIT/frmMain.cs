906c3ceadc00b7d61f2dc8843af5a923488f45ac
Naren Reddy <narendar.reddy@solutiontogo.de>
2022-03-09 17:56:26 +0530

    Submitting initial solution and documents
    Submitting initial solution and documents

---
+++ 02_Code/IIT/IIT/frmMain.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace IIT
+{
+    public partial class frmMain : DevExpress.XtraBars.Ribbon.RibbonForm
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnEntity_ItemClick(object sender, DevExpress.XtraBars.ItemClickEventArgs e)
+        {
+            
+        }
+    }
+}
