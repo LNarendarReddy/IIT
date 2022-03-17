a0a1d17e3cf06b3d67d9f9cc27d90c55af4ce115
sainathgoud504 <31504412+sainathgoud504@users.noreply.github.com>
2022-03-17 21:07:12 +0530

    created folder structure

--- 02_Code/IIT/IIT/frmMain.cs
+++ 02_Code/IIT/IIT/frmMain.cs
@@ -1,13 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Windows.Forms;
-
-namespace IIT
+﻿namespace IIT
 {
     public partial class frmMain : DevExpress.XtraBars.Ribbon.RibbonForm
     {
