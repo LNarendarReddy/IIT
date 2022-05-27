7e29067cf780f0a5f5ce23cfe7cbb80e3adf63df
sainathgoud504 <31504412+sainathgoud504@users.noreply.github.com>
2022-05-27 13:22:02 +0530

    Update ucLedgerReport.Designer.cs

--- 02_Code/IIT/IIT/Ledger/ucLedgerReport.Designer.cs
+++ 02_Code/IIT/IIT/Ledger/ucLedgerReport.Designer.cs
@@ -31,14 +31,6 @@
             this.tablePanel1 = new DevExpress.Utils.Layout.TablePanel();
             this.gcVouchers = new DevExpress.XtraGrid.GridControl();
             this.gvVouchers = new DevExpress.XtraGrid.Views.Grid.GridView();
-            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
-            this.lblClosingBalance = new DevExpress.XtraEditors.LabelControl();
-            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
-            this.lblOpeningBalance = new DevExpress.XtraEditors.LabelControl();
-            this.lblLedgerName = new DevExpress.XtraEditors.LabelControl();
-            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
-            this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
-            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
             this.gridColumn1 = new DevExpress.XtraGrid.Columns.GridColumn();
             this.gridColumn2 = new DevExpress.XtraGrid.Columns.GridColumn();
             this.gridColumn3 = new DevExpress.XtraGrid.Columns.GridColumn();
@@ -50,6 +42,14 @@
             this.gridColumn9 = new DevExpress.XtraGrid.Columns.GridColumn();
             this.gridColumn10 = new DevExpress.XtraGrid.Columns.GridColumn();
             this.gridColumn11 = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.labelControl5 = new DevExpress.XtraEditors.LabelControl();
+            this.lblClosingBalance = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.lblOpeningBalance = new DevExpress.XtraEditors.LabelControl();
+            this.lblLedgerName = new DevExpress.XtraEditors.LabelControl();
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
             ((System.ComponentModel.ISupportInitialize)(this.tablePanel1)).BeginInit();
             this.tablePanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gcVouchers)).BeginInit();
@@ -118,92 +118,6 @@
             this.gridColumn11});
             this.gvVouchers.GridControl = this.gcVouchers;
             this.gvVouchers.Name = "gvVouchers";
-            // 
-            // labelControl5
-            // 
-            this.tablePanel1.SetColumn(this.labelControl5, 3);
-            this.labelControl5.Location = new System.Drawing.Point(544, 436);
-            this.labelControl5.Name = "labelControl5";
-            this.tablePanel1.SetRow(this.labelControl5, 4);
-            this.labelControl5.Size = new System.Drawing.Size(91, 15);
-            this.labelControl5.TabIndex = 4;
-            this.labelControl5.Text = "Closing Balance";
-            // 
-            // lblClosingBalance
-            // 
-            this.tablePanel1.SetColumn(this.lblClosingBalance, 4);
-            this.lblClosingBalance.Location = new System.Drawing.Point(654, 436);
-            this.lblClosingBalance.Name = "lblClosingBalance";
-            this.tablePanel1.SetRow(this.lblClosingBalance, 4);
-            this.lblClosingBalance.Size = new System.Drawing.Size(24, 15);
-            this.lblClosingBalance.TabIndex = 3;
-            this.lblClosingBalance.Text = "0.00";
-            // 
-            // labelControl3
-            // 
-            this.tablePanel1.SetColumn(this.labelControl3, 3);
-            this.labelControl3.Location = new System.Drawing.Point(544, 41);
-            this.labelControl3.Name = "labelControl3";
-            this.tablePanel1.SetRow(this.labelControl3, 2);
-            this.labelControl3.Size = new System.Drawing.Size(104, 15);
-            this.labelControl3.TabIndex = 2;
-            this.labelControl3.Text = "Opening Balance : ";
-            // 
-            // lblOpeningBalance
-            // 
-            this.tablePanel1.SetColumn(this.lblOpeningBalance, 4);
-            this.lblOpeningBalance.Location = new System.Drawing.Point(654, 41);
-            this.lblOpeningBalance.Name = "lblOpeningBalance";
-            this.tablePanel1.SetRow(this.lblOpeningBalance, 2);
-            this.lblOpeningBalance.Size = new System.Drawing.Size(24, 15);
-            this.lblOpeningBalance.TabIndex = 1;
-            this.lblOpeningBalance.Text = "0.00";
-            // 
-            // lblLedgerName
-            // 
-            this.lblLedgerName.Appearance.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
-            this.lblLedgerName.Appearance.Options.UseFont = true;
-            this.lblLedgerName.Appearance.Options.UseTextOptions = true;
-            this.lblLedgerName.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
-            this.tablePanel1.SetColumn(this.lblLedgerName, 1);
-            this.tablePanel1.SetColumnSpan(this.lblLedgerName, 5);
-            this.lblLedgerName.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.lblLedgerName.Location = new System.Drawing.Point(13, 13);
-            this.lblLedgerName.Name = "lblLedgerName";
-            this.tablePanel1.SetRow(this.lblLedgerName, 1);
-            this.lblLedgerName.Size = new System.Drawing.Size(835, 22);
-            this.lblLedgerName.TabIndex = 0;
-            this.lblLedgerName.Text = "labelControl1";
-            // 
-            // layoutControl1
-            // 
-            this.layoutControl1.Controls.Add(this.tablePanel1);
-            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
-            this.layoutControl1.Name = "layoutControl1";
-            this.layoutControl1.Root = this.Root;
-            this.layoutControl1.Size = new System.Drawing.Size(885, 488);
-            this.layoutControl1.TabIndex = 1;
-            this.layoutControl1.Text = "layoutControl1";
-            // 
-            // Root
-            // 
-            this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
-            this.Root.GroupBordersVisible = false;
-            this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
-            this.layoutControlItem1});
-            this.Root.Name = "Root";
-            this.Root.Size = new System.Drawing.Size(885, 488);
-            this.Root.TextVisible = false;
-            // 
-            // layoutControlItem1
-            // 
-            this.layoutControlItem1.Control = this.tablePanel1;
-            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
-            this.layoutControlItem1.Name = "layoutControlItem1";
-            this.layoutControlItem1.Size = new System.Drawing.Size(865, 468);
-            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
-            this.layoutControlItem1.TextVisible = false;
             // 
             // gridColumn1
             // 
@@ -282,10 +196,110 @@
             // gridColumn11
             // 
             this.gridColumn11.Caption = "Balance";
-            this.gridColumn11.FieldName = "RUNNNINGBAL";
+            this.gridColumn11.FieldName = "RUNNINGBAL";
             this.gridColumn11.Name = "gridColumn11";
             this.gridColumn11.Visible = true;
             this.gridColumn11.VisibleIndex = 7;
+            // 
+            // labelControl5
+            // 
+            this.labelControl5.Appearance.Options.UseTextOptions = true;
+            this.labelControl5.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Far;
+            this.tablePanel1.SetColumn(this.labelControl5, 3);
+            this.labelControl5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelControl5.Location = new System.Drawing.Point(544, 436);
+            this.labelControl5.Name = "labelControl5";
+            this.tablePanel1.SetRow(this.labelControl5, 4);
+            this.labelControl5.Size = new System.Drawing.Size(104, 15);
+            this.labelControl5.TabIndex = 4;
+            this.labelControl5.Text = "Closing Balance : ";
+            // 
+            // lblClosingBalance
+            // 
+            this.lblClosingBalance.Appearance.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.lblClosingBalance.Appearance.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lblClosingBalance.Appearance.Options.UseFont = true;
+            this.lblClosingBalance.Appearance.Options.UseForeColor = true;
+            this.tablePanel1.SetColumn(this.lblClosingBalance, 4);
+            this.lblClosingBalance.Location = new System.Drawing.Point(654, 436);
+            this.lblClosingBalance.Name = "lblClosingBalance";
+            this.tablePanel1.SetRow(this.lblClosingBalance, 4);
+            this.lblClosingBalance.Size = new System.Drawing.Size(24, 15);
+            this.lblClosingBalance.TabIndex = 3;
+            this.lblClosingBalance.Text = "0.00";
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Appearance.Options.UseTextOptions = true;
+            this.labelControl3.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Far;
+            this.tablePanel1.SetColumn(this.labelControl3, 3);
+            this.labelControl3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelControl3.Location = new System.Drawing.Point(544, 41);
+            this.labelControl3.Name = "labelControl3";
+            this.tablePanel1.SetRow(this.labelControl3, 2);
+            this.labelControl3.Size = new System.Drawing.Size(104, 15);
+            this.labelControl3.TabIndex = 2;
+            this.labelControl3.Text = "Opening Balance : ";
+            // 
+            // lblOpeningBalance
+            // 
+            this.lblOpeningBalance.Appearance.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Bold);
+            this.lblOpeningBalance.Appearance.ForeColor = System.Drawing.Color.SteelBlue;
+            this.lblOpeningBalance.Appearance.Options.UseFont = true;
+            this.lblOpeningBalance.Appearance.Options.UseForeColor = true;
+            this.tablePanel1.SetColumn(this.lblOpeningBalance, 4);
+            this.lblOpeningBalance.Location = new System.Drawing.Point(654, 41);
+            this.lblOpeningBalance.Name = "lblOpeningBalance";
+            this.tablePanel1.SetRow(this.lblOpeningBalance, 2);
+            this.lblOpeningBalance.Size = new System.Drawing.Size(24, 15);
+            this.lblOpeningBalance.TabIndex = 1;
+            this.lblOpeningBalance.Text = "0.00";
+            // 
+            // lblLedgerName
+            // 
+            this.lblLedgerName.Appearance.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold);
+            this.lblLedgerName.Appearance.Options.UseFont = true;
+            this.lblLedgerName.Appearance.Options.UseTextOptions = true;
+            this.lblLedgerName.Appearance.TextOptions.HAlignment = DevExpress.Utils.HorzAlignment.Center;
+            this.tablePanel1.SetColumn(this.lblLedgerName, 1);
+            this.tablePanel1.SetColumnSpan(this.lblLedgerName, 5);
+            this.lblLedgerName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.lblLedgerName.Location = new System.Drawing.Point(13, 13);
+            this.lblLedgerName.Name = "lblLedgerName";
+            this.tablePanel1.SetRow(this.lblLedgerName, 1);
+            this.lblLedgerName.Size = new System.Drawing.Size(835, 22);
+            this.lblLedgerName.TabIndex = 0;
+            this.lblLedgerName.Text = "labelControl1";
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.Controls.Add(this.tablePanel1);
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.Root = this.Root;
+            this.layoutControl1.Size = new System.Drawing.Size(885, 488);
+            this.layoutControl1.TabIndex = 1;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // Root
+            // 
+            this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.Root.GroupBordersVisible = false;
+            this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1});
+            this.Root.Name = "Root";
+            this.Root.Size = new System.Drawing.Size(885, 488);
+            this.Root.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.tablePanel1;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(865, 468);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextVisible = false;
             // 
             // ucLedgerReport
             // 
